92ff57a7fe90c28f050243bc8833b05dd5faeb9f
Gouthami Paruchuri <gouthami_paruchuri@dell.com>
2025-09-09 21:24:19 +0530

    React through NPM with Parcel

--- Readme.md
+++ Readme.md
@@ -1,1 +1,21 @@
-React !!!
+1. React integration in existing JS.
+===================================================================================================================================
+2. Parcel
+- Dev Build
+- Local Server
+- HMR/hot reloading = hot module replacement(refresh app immediately on save)
+- file watching algorithm - c++
+- caching - faster builds
+- image optimization
+- minification
+- bundling
+- compress
+- consistant hashing
+- code splitting
+- differential bundling
+- diagnostics
+- https
+- tree shaking algorithm(remove unused algorithm)
+- different dev and prod builds
+- transpiled (babel)
+====================================================================================================================================
--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var heading = React.createElement("h1", {id: "h1Header", "hello": "world"}, "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+//React
+var heading = React.createElement("h1", { key: "h1Header", "hello": "world" }, "Hello World from React!");
 var root = ReactDOM.createRoot(document.getElementById("reactHello"));
 root.render(heading);
 
 
-var nestedElement = React.createElement("div", {id: "mainDiv"}, [
-                        React.createElement("div", {id: "nestedDiv1"}, 
-                            React.createElement("h1", {id:"header1"}, "Nested header1")
-                        ),
-                        React.createElement("div", {id: "nestedDiv2"}, 
-                            React.createElement("h1", {id:"header2"}, "Nested header2")
-                        )
-                    ])
+var nestedElement = React.createElement("div", { key: "mainDiv" }, [
+    React.createElement("div", { key: "nestedDiv1" },
+        React.createElement("h1", { key: "header1" }, "Nested header1")
+    ),
+    React.createElement("div", { key: "nestedDiv2" },
+        React.createElement("h1", { key: "header2" }, "Nested header2")
+    )
+])
 ReactDOM.createRoot(document.getElementById("nestedDiv")).render(nestedElement);
